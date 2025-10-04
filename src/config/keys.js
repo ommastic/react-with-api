396a2a50d1b79a7104b897463535e24d36881d98
muheez <muheezopebiyi@gmail.com>
2025-10-03 23:02:37 -0400

    updated to vite

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,6 +1,6 @@
-export const API_KEY = process.env.VITE_OMDB_KEY
-export const TMDB_KEY = process.env.VITE_TMDB_KEY
+export const API_KEY = import.meta.env.VITE_OMDB_KEY || ''
+export const TMDB_KEY = import.meta.env.VITE_TMDB_KEY || ''
 
 if (!API_KEY || !TMDB_KEY){
-	console.warn("Missing OMDB_KEY or TMDB_KEY")
+	console.warn("Missing OMDB_KEY or TMDB_KEY in .env.local")
 }
